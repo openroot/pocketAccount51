a590ae5242f249a91ffb3b39df15c47083392987
Debaprasad Tapader <dev.openroot@gmail.com>
2024-11-01 13:37:15 +0530

    change

--- . Pocket (Account)/. Excess (Initial)/. Excess (Initial).php
+++ . Pocket (Account)/. Excess (Initial)/. Excess (Initial).php
@@ -1,3 +1,39 @@
 <?php
-	echo "live";
+	class readFile {
+		private string $name = "";
+		private ?int $size = null;
+		private ?array $content = null;
+
+		function __construct(string $name) {
+			$this->name = $name;
+			$this->read();
+		}
+
+		private function read(): void {
+			$file = null;
+			$file = fopen($this->name, "r");
+			if ($file) {
+				$this->size = filesize($this->name);
+				if ($this->size > 0) {
+					$this->content = [];
+					while (!feof($file)) {
+						array_push($this->content, fgets($file));
+					}
+					fclose($file);
+				}
+			}
+		}
+
+		public function fromName(): string {
+			return $this->name;
+		}
+
+		public function fromSize(): ?int {
+			return $this->size;
+		}
+
+		public function fromContent(): ?array {
+			return $this->content;
+		}
+	}
 ?>
